4a9a0a7d4ad31a277bf12dfca5c0ef3a2ddd10cc
Amanpandey046 <pandey.046aman@gmail.com>
2022-09-06 10:19:01 +0530

    SEP6

--- u.py
+++ u.py
@@ -1,6 +1,12 @@
 x = int(input("Enter the Number :"))
-
-for i in range(x+1):
-	for j in range(x,i,-1):
-		print("*",end = " ")
-	print()
+a = 0
+b = 1
+sum = 0
+count = 1
+print("The Fibonacci series of ",x,"is :")
+while(count<=x):
+	print(sum,end= " ")
+	count += 1
+	a = b
+	b = sum
+	sum = (a+b)
